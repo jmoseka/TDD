370effdbffd3a8fa9935237e0d107d4fcec3d977
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-07-28 15:59:45 +0800

    Write unit tests for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,1 +1,19 @@
 require_relative '../solver'
+
+describe 'Write unit tests for class Solver' do
+  before :each do
+    @solver = Solver.new
+  end
+
+  context '#factorial method' do
+    it 'should take one argument N and returns the factorial for that number' do
+      result = @solver.factorial(0)
+      expect(result).to eql 1
+    end
+
+    it 'should return 720 when N is equal to 6' do
+      result = @solver.factorial(6)
+      expect(result).to eql 720
+    end
+  end
+end
