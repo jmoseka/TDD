70677f719418ba36a0ab336c5d370f87019df23f
jmoseka <jamylam3@gmail.com>
2022-07-28 11:38:07 +0300

    Unit test for fizzbuzz function

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -21,4 +21,20 @@ describe 'Write unit tests for class Solver' do
       expect(result).to match('No negative integer. This is an exception.')
     end
   end
+
+    context '#fizzbuzz function' do
+      it 'should return fizzbuzz if the number is divisible by 3 and 5' do
+        expect(@solver.fizzbuzz(15)).to eq 'fizzbuzz'
+      end
+      it 'should return a fizz if the number is divisible by 3' do
+        expect(@solver.fizzbuzz(3)).to eq 'fizz'
+      end
+      it 'should return a buzz if the number is divisible by 5' do
+        expect(@solver.fizzbuzz(5)).to eq 'buzz'
+      end
+      it 'should return a number if the number is not divisible by 3 or 5' do
+        expect(@solver.fizzbuzz(7)).to eq 7
+      end
+    end
+
 end
